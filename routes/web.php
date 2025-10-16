83665d7302b6512fc5025517c50d09a2321c5bb3
AlyBautista <bautista.alyssar16@gmail.com>
2025-10-16 15:15:09 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+#added
+use App\Http\Controllers\HomeController;
+
+
+#added
+Route::get('/',[HomeController::class,'my_home']);
+
+Route::get('/home',[HomeController::class,'index']);
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
